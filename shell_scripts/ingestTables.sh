0c840913e5f5d8cedaa55c2b45e7e6543561be48
andrewhywong <hywong@utexas.edu>
2022-12-10 15:17:55 -0600

    add shell script to batch ingest csv to GEE

---
+++ shell_scripts/ingestTables.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#cat occ_list | while read line 
+#do
+#   echo $line
+#done
+
+exec 3<occName_list
+exec 4<occ_list
+asset="users/hywong/occ_gcloud/"
+#gcloud_pre="gs://nymphs_bucket_occ/"
+
+while read occName <&3 && read occData <&4
+do
+        earthengine upload table --asset_id $asset$occName --x_column decimalLongitude --y_column decimalLatitude --crs EPSG:4326 $occData
+	echo $asset$occName
+done
+exec 3<&-
+exec 4<&-
