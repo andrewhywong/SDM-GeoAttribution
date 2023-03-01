d5bd69ac95e6c3b004289fa4063b5890a30306e4
andrewhywong <hywong@utexas.edu>
2023-03-01 14:10:48 -0600

    Cleaned locations of output files

--- shell_scripts/ingestTables.sh
+++ shell_scripts/ingestTables.sh
@@ -1,22 +1,22 @@
 #!/bin/sh
 
 # change source gcp occ location:
-target_files="gs://nymphs_bucket_occ/CASES/stern_global_reduced/*.csv"
+target_files="gs://nymphs_bucket_occ/occdata_rmFull_Global_raw_reduced/*.csv"
 
 # change target GEE asset folder to ingest:
-asset="users/hywong/occ_stern_global/"
+asset="users/hywong/target_folder/"
 
-gsutil ls $target_files | sed 's:.*/::' | cut -d '.' -f 1 > ./occNamedelete
-gsutil ls $target_files > ./occdelete
+gsutil ls $target_files | sed 's:.*/::' | cut -d '.' -f 1 > ./occNames
+gsutil ls $target_files > ./occLoc
 
 #echo ./occdelete
 
-exec 3<./occNamedelete
-exec 4<./occdelete
+exec 3<./occNames
+exec 4<./occLoc
 
 while read occName <&3 && read occData <&4
 do
-        earthengine upload table --asset_id $asset$occName --x_column decimalLongitude --y_column decimalLatitude --crs EPSG:4326 $occData
+        #earthengine upload table --asset_id $asset$occName --x_column decimalLongitude --y_column decimalLatitude --crs EPSG:4326 $occData
 	echo $asset$occName
 done
 
