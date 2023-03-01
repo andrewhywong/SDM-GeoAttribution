f922bbee6bdf4c147b3734cb7a2d671b4fc60ca3
andrewhywong <hywong@utexas.edu>
2023-03-01 13:56:07 -0600

    Update GEE ingest

--- shell_scripts/ingestTables.sh
+++ shell_scripts/ingestTables.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-target_files="gs://nymphs_bucket_occ/doe_occ/*.csv"
+# change source gcp occ location:
+target_files="gs://nymphs_bucket_occ/CASES/stern_global_reduced/*.csv"
+
+# change target GEE asset folder to ingest:
+asset="users/hywong/occ_stern_global/"
 
 gsutil ls $target_files | sed 's:.*/::' | cut -d '.' -f 1 > ./occNamedelete
 gsutil ls $target_files > ./occdelete
@@ -10,13 +14,11 @@ gsutil ls $target_files > ./occdelete
 exec 3<./occNamedelete
 exec 4<./occdelete
 
-# change target GEE asset folder to ingest:
-asset="users/hywong/occ_doe/"
-
 while read occName <&3 && read occData <&4
 do
         earthengine upload table --asset_id $asset$occName --x_column decimalLongitude --y_column decimalLatitude --crs EPSG:4326 $occData
 	echo $asset$occName
 done
+
 exec 3<&-
 exec 4<&-
