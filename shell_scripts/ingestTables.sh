2c9b15d34ba1d0fe70297c641d65c6fb580248f6
andrewhywong <hywong@utexas.edu>
2023-03-08 00:17:07 -0600

    Check shell scripts

--- shell_scripts/ingestTables.sh
+++ shell_scripts/ingestTables.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # change source gcp occ location:
-target_files="gs://nymphs_bucket_occ/occdata_rmFull_Global_raw_reduced/*.csv"
+target_files="gs://nymphs_bucket_occ/occdata_rmFull_cloth_global/*.csv"
 
 # change target GEE asset folder to ingest:
-asset="users/hywong/target_folder/"
+asset="users/hywong/occ_nymphs_cloth_global/"
 
 gsutil ls $target_files | sed 's:.*/::' | cut -d '.' -f 1 > ./occNames
 gsutil ls $target_files > ./occLoc
@@ -16,7 +16,7 @@ exec 4<./occLoc
 
 while read occName <&3 && read occData <&4
 do
-        #earthengine upload table --asset_id $asset$occName --x_column decimalLongitude --y_column decimalLatitude --crs EPSG:4326 $occData
+        earthengine upload table --asset_id $asset$occName --x_column decimalLongitude --y_column decimalLatitude --crs EPSG:4326 $occData
 	echo $asset$occName
 done
 
